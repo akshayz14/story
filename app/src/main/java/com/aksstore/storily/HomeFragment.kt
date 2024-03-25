5f0c650ac9719e8547bf94ff764db78c2e1ef9b1
akshayz14 <work.akshay@gmail.com>
2024-03-25 13:50:32 +0530

    Adding content in jsons

--- app/src/main/java/com/aksstore/storily/HomeFragment.kt
+++ app/src/main/java/com/aksstore/storily/HomeFragment.kt
@@ -304,7 +304,7 @@ class HomeFragment : Fragment() {
 
 
             AppConstants.JOKES_STORIES -> {
-                navigateToListPage("JOKES")
+                navigateToListPage("FUNNY")
             }
 
 
