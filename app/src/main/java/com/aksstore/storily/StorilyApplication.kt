2e2c37528bfdcd0f323014c2eafdb9e70ae4cc7f
akshayz14 <work.akshay@gmail.com>
2024-09-04 12:08:46 +0530

    Adding app context

--- app/src/main/java/com/aksstore/storily/StorilyApplication.kt
+++ app/src/main/java/com/aksstore/storily/StorilyApplication.kt
@@ -1,7 +1,23 @@
 package com.aksstore.storily
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class StorilyApplication : Application()
+class StorilyApplication : Application() {
+
+    companion object {
+        private lateinit var instance: StorilyApplication
+
+        fun getAppContext(): Context {
+            return instance.applicationContext
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+}
