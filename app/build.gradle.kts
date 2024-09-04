e3f51c96f94e6f6390b809d1e8c852daa2b6c836
akshayz14 <work.akshay@gmail.com>
2024-09-04 14:04:04 +0530

    version code and version name bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aksstore.storily"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
