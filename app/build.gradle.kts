cc4913897ea73c333444771566f0734700f14bf3
akshayz14 <work.akshay@gmail.com>
2026-01-20 19:31:46 +0530

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aksstore.storily"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.2"
+        versionCode = 7
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
